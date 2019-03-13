996f9ff3c24d3c28fffac308dcc35d8ec9a86e1e
Rahmawati Asy syifa' Putri 27 RPL <42015213+Rahmawatiasysyifaputri@users.noreply.github.com>
2019-03-13 11:46:09 +0700

    Add files via upload

---
+++ form-create.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Data Dosen</title>
+  </head>
+  <body>
+    <form action ="create.php" method="post">
+        Nama Dosen:<input type="text" name="nama_dosen"><br>
+        <br>
+        No. Telepon:<input type="text" name="telp"><br>
+        <br>
+        <input type="submit" name="btnSimpan" value="Simpan">
+    </form>
+  </body>
+</html>
